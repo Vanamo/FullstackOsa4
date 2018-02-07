f018690af14d6ac3ffe865c12c3a882067748b62
Piirainen Vanamo K <vseppane@da5-cs-bk107-36.pc.helsinki.fi>
2018-02-07 16:37:55 +0200

    4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -42,21 +42,42 @@ blogsRouter.post('/', async (request, response) => {
 
     const savedBlog = await blog.save()
 
-    response.status(201).json(blog)
+    response.status(201).json(savedBlog)
   } catch (exception) {
     console.log(exception)
     response.status(500).json({ error: 'something went wrong' })
   }
 })
+
 blogsRouter.delete('/:id', async (request, response) => {
   try {
-    console.log('id: ', request.params.id)
     await Blog.findByIdAndRemove(request.params.id)
 
     response.status(204).end()
   } catch (exception) {
     console.log(exception)
     response.status(400).send({ error: 'malformatted id' })
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  try {
+    const body = request.body
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+
+    console.log('id: ', request.params.id)
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+
+    response.status(200).json(updatedBlog)
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'malforamatted id' })
   }
 })
 
